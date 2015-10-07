508372c978644f180b177487f09185c5d46b8ed1
MohDid <Piranesis@gmail.com>
2015-10-07 16:47:39 +0200

    Update robotic.py

--- robotic.py
+++ robotic.py
@@ -33,7 +33,7 @@ def check_distance():
 	distance = round(distance, 2)
 	
 	print "Distance:",distance,"cm"
-	time.sleep(1)
+	time.sleep(0.1)
 	return distance;
 
  
@@ -150,14 +150,14 @@ while True:
 
 	if (distance >= 10):
 		avancer()
-		time.sleep(1)
+		time.sleep(0.5)
 	else:
 		while (distance < 10):
 			droite()
 			distance = check_distance()
 			time.sleep(0.0001)
 		avancer()
-		time.sleep(1)
+		time.sleep(0.5)
 GPIO.cleanup()
 
 	
